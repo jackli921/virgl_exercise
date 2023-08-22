f274733b755f09b5a7a55d84bdd12f0597105766
Run Qi (Jack) Li <li.jack0707@gmail.com>
2023-08-22 18:17:32 -0400

    refactor: remove data fetching logic from pages.tsx to display component

--- app/components/CurrentTempContainer.tsx
+++ app/components/CurrentTempContainer.tsx
@@ -1,24 +1,48 @@
-// components/CurrentTempContainer.tsx
-
-import React from "react";
+"use client";
+import React, { useState, useEffect } from "react";
 import AutoUpdateButton from "./AutoUpdateButton";
 import { GetWeatherData } from "../types/apiResponses"; // Update the path to apiResponses
 
-interface CurrentTempContainerProps {
-  data: GetWeatherData; // Define the prop type
-}
+interface CurrentTempContainerProps {}
 
-const CurrentTempContainer: React.FC<CurrentTempContainerProps> = ({
-  data,
-}) => {
+const CurrentTempContainer: React.FC<CurrentTempContainerProps> = () => {
   const cityName = "New York";
-  const {timezone} = data;
-  const { temperature, time } = data.current_weather;
+  const [data, setData] = useState<GetWeatherData | null>(null);
 
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const res = await fetch(
+          "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current_weather=true&hourly=temperature_2m"
+        );
+
+        if (!res.ok) {
+          throw new Error("Failed to fetch data");
+        }
+
+        const newData = await res.json();
+        setData(newData);
+      } catch (error) {
+        // Handle error
+      }
+    }
+
+    fetchData();
+  }, []);
+
+  if (!data) {
+    // Handle loading state
+    return <p>Loading...</p>;
+  }
+
+  const { timezone } = data;
+  const { temperature, time } = data.current_weather;
 
   return (
     <div>
-      <p>{time} {timezone}</p>
+      <p>
+        {time} {timezone}
+      </p>
       <h3>{cityName}</h3>
       <h2>{temperature}°C</h2>
       <span>Auto-Update:</span>
--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,16 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import React, { useState } from "react";
+import styles from "./page.module.css";
 import SaveButton from "./components/SaveButton";
 import ShowButton from "./components/ShowButton";
-import CurrentTempContainer from './components/CurrentTempContainer'
-import { type GetWeatherData } from './types/apiResponses'
+import CurrentTempContainer from "./components/CurrentTempContainer";
+import { GetWeatherData } from "./types/apiResponses";
 
-async function getData() {
-  const res = await fetch(
-    "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current_weather=true&hourly=temperature_2m"
-  );
-  // The return value is *not* serialized
-  // You can return Date, Map, Set, etc.
-
-  if (!res.ok) {
-    // This will activate the closest `error.js` Error Boundary
-    throw new Error("Failed to fetch data");
-  }
-
-  return res.json();
-}
-
-
-export default async function Home() {
-   const data: GetWeatherData = await getData(); // Make sure to define the type here
+export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.topContainer}>
         Top
-        <CurrentTempContainer data={data}/>
+        <CurrentTempContainer />
       </div>
 
       <div className={styles.midContainer}>mid</div>
